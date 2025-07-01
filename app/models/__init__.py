9c115c44733fd75302c59052417a7a3629c01d6a
Maurice Ngicho <mrcngicho@gmail.com>
2025-07-01 07:28:13 +0300

    Added flask_sqlalchemy

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,14 @@
-from models.engine.db_storage import DBStorage
+from app.models.engine.db_storage import DBStorage
+from flask_sqlalchemy import SQLAlchemy
 
+from app.models.user import User
+from app.models.bill import Bills
+from app.models.meter_reading import MeterReading
 
+db = SQLAlchemy
 storage = DBStorage()
+
+def init_app(app):
+    db.init_app(app)
+    
 storage.reload()
