3e3298895ef0fed93c6bf11b0fadbe20248ab659
Maurice Ngicho <mrcngicho@gmail.com>
2025-07-01 07:32:07 +0300

    Added requirements

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask.views import MethodView
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.models.user import User
 from app import storage
 
