492d38494f335d9bb3307ddcf5a2fd3423218680
Maurice Ngicho <mrcngicho@gmail.com>
2025-07-05 19:33:14 +0300

    changed table name

--- app/models/meter_reading.py
+++ app/models/meter_reading.py
@@ -5,11 +5,14 @@ from app.models.base_model import BaseModel, Base
 
 class MeterReading(BaseModel, Base):
     """MeterReading table"""
-    __tablename__ = "Meterreadings"
+    __tablename__ = "meter_readings"
 
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     reading_value = Column(Float, nullable=False)
     photo_url = Column(String(256), nullable=True)
     date = Column(DateTime, nullable=False)
 
-    bill = relationship("Bill", backref="Meterreadings", uselist=False)
+    bill = relationship("Bill", backref="meter_readings", uselist=False)
+
+    def __repr__(self):
+        return f"<MeterReading value={self.reading_value} user_id={self.user_id} date={self.date}>"
