4877b6d5288556fc1a48138ffaec4e65be79bc1f
Maurice Ngicho <mrcngicho@gmail.com>
2025-07-09 05:54:51 +0300

    modified

--- app/models/issue.py
+++ app/models/issue.py
@@ -1,6 +1,7 @@
 from app.models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from datetime import datetime
+from sqlalchemy.orm import relationship
 
 
 class Issue(BaseModel, Base):
@@ -10,3 +11,5 @@ class Issue(BaseModel, Base):
     description = Column(String(1024), nullable=False)
     status = Column(String(20), default='open', nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship('User', backref='issues', lazy='select')
