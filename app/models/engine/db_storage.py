415636c388d88c40f6d5f332dabb05f19d6e5dd0
FionaGachuuri <fionairuka@gmail.com>
2025-09-12 09:51:04 +0300

    Added db_storage.py to support Aiven MySQL with SSL

--- app/models/engine/db_storage.py
+++ app/models/engine/db_storage.py
@@ -35,6 +35,11 @@ class DBStorage:
         self.__engine = create_engine(
             f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}",
             pool_pre_ping=True
+            connect_args={
+                "ssl": {
+                    "ca": os.getenv("DB_SSL_CA", "/WaterTek/cert.pem")
+                }
+            }
         )
 
         if os.getenv("WATERTEK_ENV") == "test":
